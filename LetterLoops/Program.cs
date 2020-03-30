c1e1389216199b16710cfa2d13b25368593d39e7
Randy Tate <randytatejr@gmail.com>
2020-03-30 17:37:44 -0500

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //shotout to Monica
+            Console.WriteLine("Please enter string");
+            var userInput = Console.ReadLine();
+            var builder = "";
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                var position = i + 1;
+                for (int k = 0; k < position; k++)
+                {
+                    builder += userInput[i];
+                }
+                builder += "-";
+            }
+            char[] charToTrim = { '-' };
+            var output = builder.TrimEnd(charToTrim);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
